5815eb4ecb2b7be90db9c26201656d77015401a1
bradynp <bpbraithwaite@gmail.com>
2022-01-29 16:54:16 -0700

    add color blind mode

--- splashtext.h
+++ splashtext.h
@@ -20,13 +20,13 @@ static const std::vector<std::string> Splash = {
 string watermark = "C++ clone for CS225 - 2022 by B.P. Braithwaite";
 namespace color {
 	string white = "\033[0m";
+	string black = "\033[30m";
+	string grey = "\033[90m";
+	string red = "\033[91m";
+	string yellow = "\033[93m";
+	string green = "\033[92m";
 	string blue = "\033[34m";
 	string lightblue = "\033[36m";
-	string green = "\033[92m";
-	string yellow = "\033[93m";
-	string red = "\033[91m";
-	string grey = "\033[90m";
-	string black = "\033[30m";
 }
 string clearscreen = true ? "\033c" : ""; // make false to see scrollback
 string blocktab = "\t\t\t\t\t\t";
